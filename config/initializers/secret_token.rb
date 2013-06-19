0f128e6ea03c0e5c27527500a88973e142cdd928
Jessie Keck <jessie.keck@gmail.com>
2013-06-19 14:25:56 -0700

    Initial commit of bare rails4 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeliCat::Application.config.secret_key_base = '742307032847f9cd9b9dc18cf69847472adc22900ff476be9188310008efbab2770eecec93cd6db3fb5e5a8300f07d67cbfa2aa79e82f8a81ab7ab5b13c79711'
