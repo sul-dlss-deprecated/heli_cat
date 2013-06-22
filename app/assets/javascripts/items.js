f341585f4436e6bac52c3c258c5d18a22e4e8800
Jessie Keck <jessie.keck@gmail.com>
2013-06-22 01:37:52 -0700

    Add express service code field and toggle it w/ jQuery when the Make attribute is 'Dell'

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
 	$("#item_department").typeahead({source: ["DPG", "Lyberteam", "PSM", "Webteam"]})
 	$("#item_make").typeahead({source: ["Dell", "Mac"]});
 	$("#item_swap_cycle").typeahead({source: ["1-year", "2-years", "3-years"]});
+
+	var service_code_parent = $("#item_express_service_code").closest(".control-group");
+	service_code_parent.hide();
+	$("#item_make").change(function(){
+		if($(this).prop("value") == "Dell") {
+			service_code_parent.slideToggle();
+		}else{
+			service_code_parent.slideToggle();
+		}
+	});
 	
 	$("#item_barcode").keyup(function(){
 		if($(this).prop("value").length == 2) {
