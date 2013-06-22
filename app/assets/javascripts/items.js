6bc43a8042895d728e9b55e3d3eb5c6885514d6c
Jessie Keck <jessie.keck@gmail.com>
2013-06-22 01:29:11 -0700

    Add extra zeros into barcode field once there are 2 characters.

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
 	$("#item_department").typeahead({source: ["DPG", "Lyberteam", "PSM", "Webteam"]})
 	$("#item_make").typeahead({source: ["Dell", "Mac"]});
 	$("#item_swap_cycle").typeahead({source: ["1-year", "2-years", "3-years"]});
+	
+	$("#item_barcode").keyup(function(){
+		if($(this).prop("value").length == 2) {
+			$(this).prop("value", $(this).prop("value") + "00000000");
+		}
+	});
 });
