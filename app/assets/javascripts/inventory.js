969de1eea5d679e2d423c37d6bcdf3d8d256c34a
Jessie Keck <jessie.keck@gmail.com>
2013-07-26 12:36:07 -0700

    Automatically check the 'purchased' checkbox when the 'received' checkbox is checked.

--- app/assets/javascripts/inventory.js
+++ app/assets/javascripts/inventory.js
@@ -9,5 +9,19 @@ function tooltips(){
 		$(this).tooltip();
 	});
 }
-$(document).ready(function(){data_typeahead_options(); tooltips()});
-document.addEventListener("page:load", function(){data_typeahead_options(); tooltips()});
+function purchasing_workflow_checkboxes(){
+	var purchased = $("input[type='checkbox']#item_purchased");
+	var received = $("input[type='checkbox']#item_received");
+	received.on("change", function(){
+		if($(this).prop("checked")){
+		  purchased.prop("checked", true);
+		}
+	});
+	purchased.on("change", function(){
+		if(!$(this).prop("checked")){
+		  received.prop("checked", false);
+		}
+	});
+}
+$(document).ready(function(){data_typeahead_options(); tooltips(); purchasing_workflow_checkboxes()});
+document.addEventListener("page:load", function(){data_typeahead_options(); tooltips(); purchasing_workflow_checkboxes()});
