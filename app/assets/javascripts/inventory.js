4401e3ddd69260720bd54554699a1bee1350e9b7
Jessie Keck <jessie.keck@gmail.com>
2013-08-07 11:48:27 -0700

    Update jQuery to use .on('ready page:load') instead of invoking the function twice.

--- app/assets/javascripts/inventory.js
+++ app/assets/javascripts/inventory.js
@@ -1,15 +1,11 @@
-function data_typeahead_options() {
+jQuery(document).on("ready page:load", function(){
 	$("[data-typeahead-options]").each(function(){
 		var options = $(this).data("typeahead-options").split(",");
 		$(this).prop("autocomplete", "off").typeahead({source: options});
 	});
-}
-function tooltips(){
 	$("[data-toggle='tooltip']").each(function(){
 		$(this).tooltip();
 	});
-}
-function purchasing_workflow_checkboxes(){
 	var purchased = $("input[type='checkbox']#item_purchased");
 	var received = $("input[type='checkbox']#item_received");
 	received.on("change", function(){
@@ -21,7 +17,5 @@ function purchasing_workflow_checkboxes(){
 		if(!$(this).prop("checked")){
 		  received.prop("checked", false);
 		}
-	});
-}
-$(document).ready(function(){data_typeahead_options(); tooltips(); purchasing_workflow_checkboxes()});
-document.addEventListener("page:load", function(){data_typeahead_options(); tooltips(); purchasing_workflow_checkboxes()});
+	});	
+});
--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,9 +1,8 @@
-function item_edit_typeahead(){
+jQuery(document).on("ready page:load", function(){
 	$("#item_department").prop("autocomplete", "off").typeahead({source: ["DPG", "Lyberteam", "PSM", "Webteam"]})
 	$("#item_make").prop("autocomplete", "off").typeahead({source: ["Dell", "Mac"]});
 	$("#item_swap_cycle").prop("autocomplete", "off").typeahead({source: ["1-year", "2-years", "3-years"]});
-}
-function handle_dell_service_code(){
+	
 	var service_code_parent = $("#item_express_service_code").closest(".control-group");
 	service_code_parent.hide();
 	$("#item_make").change(function(){
@@ -13,15 +12,11 @@ function handle_dell_service_code(){
 			service_code_parent.hide("slow");
 		}
 	});
-}
-function autofill_barcode(){
 	$("#item_barcode").keyup(function(){
 		if($(this).prop("value").length == 2) {
 			$(this).prop("value", $(this).prop("value") + "00000000");
 		}
 	});
-}
-function swap_value_for_current_user(){
 	$("[data-toggle='current-user']").on("click", function(){
 		var link = $(this);
 		var user = $(this).attr("data-current-user");
@@ -29,6 +24,4 @@ function swap_value_for_current_user(){
 		target.attr("value", user);
 		return false;
 	});
-}
-$(document).ready(function(){item_edit_typeahead(); handle_dell_service_code(); autofill_barcode(); swap_value_for_current_user()});
-document.addEventListener("page:load", function(){item_edit_typeahead(); handle_dell_service_code(); autofill_barcode(); swap_value_for_current_user()});
+});
