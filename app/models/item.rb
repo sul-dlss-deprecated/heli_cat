8adf8a57f74f3adbf87eaa20b33b042510d8261c
Jessie Keck <jessie.keck@gmail.com>
2013-08-09 11:39:22 -0700

    Add blank values for swap cycle so it's not default set to 1 month and add validations and tests for swap cycle fields.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,27 @@
 class Item < ActiveRecord::Base
   has_one :purchase_option
+  validates :swap_cycle, format: {with: /|\d{1}-years|months|/, message: "Swap cycle not valid"}
+
+  validates :swap_cycle_number, inclusion: { in: [""].concat(("1"..."7").to_a),
+                                             message: "%{value} is not a valid number" }
+  validates :swap_cycle_span,   inclusion: { in: [nil, "years", "months"],
+                                             message: "%{value} is not a valid date unit" }
   def swap_cycle_number=(number)
     self.swap_cycle = "#{number}-#{swap_cycle_span}"
     self.save!
   end
+
   def swap_cycle_span=(span)
     self.swap_cycle = "#{swap_cycle_number}-#{span}"
     self.save!
   end
   def swap_cycle_number
     return nil unless swap_cycle
-    swap_cycle.split("-").first
+    swap_cycle.split("-")[0]
   end
   def swap_cycle_span
     return nil unless swap_cycle
-    swap_cycle.split("-").last
+    swap_cycle.split("-")[1]
   end
   def destroy
     swappable_items = self.class.where(swap_item: self.id)
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,5 +14,17 @@ describe Item do
       item = Item.create!(swap_cycle_number: "3", swap_cycle_span: "years")
       expect(item.swap_cycle).to eq "3-years"
     end
+    describe "validations" do
+      it "should not allow a bad value in the swap_cycle_number" do
+        -> {Item.create!(swap_cycle_number: "10")}.should raise_error ActiveRecord::RecordInvalid
+      end
+      it "should not allow a bad value in the swap_cycle_span" do
+        -> {Item.create!(swap_cycle_span: "centuries")}.should raise_error ActiveRecord::RecordInvalid
+      end
+      it "should not allow badly formed swap_cycles" do
+        -> {Item.create!(swap_cycle: "100-years")}.should raise_error ActiveRecord::RecordInvalid
+        -> {Item.create!(swap_cycle: "5-centuries")}.should raise_error ActiveRecord::RecordInvalid
+      end
+    end
   end
 end
