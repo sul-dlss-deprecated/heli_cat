4e206dae5fd824603736d6b75b29df9af2e654dc
Jessie Keck <jessie.keck@gmail.com>
2013-08-30 15:45:24 -0700

    Refactor sidebar into single navigation partial.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def render_item_options
+    if defined? @item or defined? @items
+      if defined? @item
+        render "items/item_options"
+      elsif @items.length == 1
+        render partial: "items/item_options", locals: { item: @items.first }
+      end
+    end
+  end
 end
