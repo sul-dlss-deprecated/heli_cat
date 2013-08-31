950c2c4d707f134bbd5713872800cf5daaa12490
Jessie Keck <jessie.keck@gmail.com>
2013-08-30 18:07:11 -0700

    Add admins example file and read email address from dlss_ep_email text file.

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module HeliCat
     # config.i18n.default_locale = :de
   end
 end
-HeliCat::Application.config.dlss_ep_email = "no-reply@heli-cat.stanford.edu"
+HeliCat::Application.config.dlss_ep_email = begin; File.open("#{Rails.root}/config/dlss_ep_email", "r").read.strip; rescue; "no-reply@heli-cat.stanford.edu"; end
 HeliCat::Application.config.admins = begin; YAML.load_file("#{Rails.root}/config/admins.yml")["admins"]; rescue; []; end
 
 require 'tracking'
