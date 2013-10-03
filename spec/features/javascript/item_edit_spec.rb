8833378859269c28975d0309487e60a93626329e
Jessie Keck <jessie.keck@gmail.com>
2013-10-02 18:23:44 -0700

    Add scenarios about the received and purchased checkboxes.

--- spec/features/javascript/item_edit_spec.rb
+++ spec/features/javascript/item_edit_spec.rb
@@ -7,4 +7,25 @@ feature "[me] link", js: true do
     click_link "[me]"
     find_field("item_user").value.should eq "test-user"
   end
+  scenario "should check the Purchased? checkbox when the Received? checkbox is checked" do
+    login_as "test-user"
+    visit new_item_path
+    find("#item_received").should_not be_checked
+    find("#item_purchased").should_not be_checked
+    check "Received?"
+    find("#item_received").should be_checked
+    find("#item_purchased").should be_checked
+  end
+  scenario "should uncheck the Received? checkbox when the Purchase? checkbox is unchecked" do
+    login_as "test-user"
+    visit new_item_path
+    find("#item_received").should_not be_checked
+    find("#item_purchased").should_not be_checked
+    check "Received?"
+    find("#item_received").should be_checked
+    find("#item_purchased").should be_checked
+    uncheck "Purchased?"
+    find("#item_received").should_not be_checked
+    find("#item_purchased").should_not be_checked
+  end
 end
