ef39560fc177bfad9e31f7486f9215bf0a2f1c4b
Jessie Keck <jessie.keck@gmail.com>
2013-10-04 14:32:45 -0700

    Add responsive spec for the purchase options page.

--- spec/features/javascript/responsive_spec.rb
+++ spec/features/javascript/responsive_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 feature "Responsive Design", js: true do
-  fixtures :items
+  fixtures :items, :purchase_options
   scenario "Options sidebar should hide inventory navigation for smaller viewports." do
     item = items(:macbook1)
     login_as_admin
@@ -11,5 +11,13 @@ feature "Responsive Design", js: true do
     expect(page).not_to have_selector("[data-toggle='filter-by']", visible: true)
     page.driver.resize("1024", "500")
     expect(page).to have_selector("[data-toggle='filter-by']", visible: true)
+  end
+  scenario "should hide the purcahse option number heading" do
+    purchase_a_mac
+    find("h3", text: "1)").should be_visible
+    page.driver.resize("766", "500")
+    expect(page).not_to have_selector("h3", text: "1)", visible: true)
+    page.driver.resize("1024", "500")
+    find("h3", text: "1)").should be_visible
   end
 end
